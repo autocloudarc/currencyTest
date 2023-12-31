53b8ce64d62556e22b5f250a984d81e5de9bbf62
Preston Parsard <prestopa@microsoft.com>
2023-12-31 12:53:04 -0500

    repo(transfer): from ado to ghb-public

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using pulCurrency;
+
+// issue created for error CS0246: link: https://developercommunity.visualstudio.com/t/error-CS0246:-The-type-or-namespace-name/10550647
+namespace currencyTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void convertCurrency(double amount, string currency)
+            {
+                switch (currency)
+                {
+                    case "EUR":
+                        double eurAmount = Euro.ConvertToEuro(amount);
+                        Console.WriteLine("The amount of: " + amount + " USD converted to Euros is: " + eurAmount + " " + currency);
+                        break;
+                    case "JPY":
+                        double jpyAmount = Yen.ConvertToYen(amount);
+                        Console.WriteLine("The amount of: " + amount + " USD converted to Yens is: " + jpyAmount + " " + currency);
+                        break;
+                    case "USD":
+                        Console.WriteLine("The amount of: " + amount + " USD converted to USD is: " + amount + " " + currency);
+                        break;
+                    default:
+                        Console.WriteLine("Currency not supported");
+                        break;
+                }
+            }
+            double amt = 100;
+            string euros = Euro.GetSymbol();
+            string yens = Yen.GetSymbol();
+            string dollars = USDollar.GetSymbol();
+            string[] symbolArray = new string[] { euros, yens, dollars };
+            foreach (string currency in symbolArray)
+            {
+                convertCurrency(amt, currency);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # currencyTest
-This Console Application .NET 8.0 solution tests the basic functionality of the pulCurrency.sln solution to perform currency conversions.
